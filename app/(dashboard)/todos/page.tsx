486ff4ebaf06a493b9aaf8cdcde3c1d21d312b47
Marwan Fouz <marwanfouz76@gmail.com>
2024-04-02 17:44:53 +0200

    loading & error handling

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -2,18 +2,19 @@ import db from '@/utils/db';
 import TodoList from '@/components/TodoList';
 
 const getData = async () => {
+  // await new Promise((resolve, rej) => setTimeout(() => rej(), 2000));
   const todos = await db.todo.findMany({});
   return todos;
 };
 
 const TodosPage = async () => {
   const todos = await getData();
-  console.log(todos)
+  console.log(todos);
   return (
     <div>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} />
     </div>
-  )
+  );
 };
 
 export default TodosPage;
