12b4b42a1caec8057c24c8eb9c31bea9945c5ed7
Marwan Fouz <marwanfouz76@gmail.com>
2024-04-02 22:22:02 +0200

    mark complete todos

--- components/NewTodoForm.tsx
+++ components/NewTodoForm.tsx
@@ -1,14 +1,10 @@
-'use client';
 import { newTodo } from '@/utils/actions';
-import { useState } from 'react';
 
 const NewTodoForm = () => {
-  const [state, updateState] = useState();
-
   return (
     <div>
       <form action={newTodo}>
-        <input name='content' type="text" className="border border-black" />
+        <input name="content" type="text" className="border border-black" />
         <button type="submit">new todo</button>
       </form>
     </div>
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,5 +1,6 @@
 'use client';
-
+import { useTransition } from 'react';
+import { completeTodo } from '@/utils/actions';
 import { ITodo } from '@/types';
 
 interface TodoProps {
@@ -7,7 +8,17 @@ interface TodoProps {
 }
 
 const Todo: React.FC<TodoProps> = ({ todo }) => {
-  return <div>{todo.content}</div>;
+  const [isPending, startTransition] = useTransition();
+  return (
+    <div
+      onClick={() => startTransition(() => completeTodo(todo.id))}
+      className={`px-8 py-2 border border-black/25 cursor-pointer ${
+        todo.completed ? 'line-through text-black/30' : ''
+      }`}
+    >
+      {todo.content}
+    </div>
+  );
 };
 
 export default Todo;
